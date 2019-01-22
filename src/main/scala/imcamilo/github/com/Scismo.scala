c97ae39a6f08859d04c97e8d6fa8fd6a8162d719
Camilo <camilo.jorquera.a@gmail.com>
2019-01-22 12:15:35 -0300

    patch

--- src/main/scala/imcamilo/github/com/Scismo.scala
+++ src/main/scala/imcamilo/github/com/Scismo.scala
@@ -3,7 +3,7 @@ package imcamilo.github.com
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.filter.JsonpFilter
-import com.twitter.util.{Await, Future}
+import com.twitter.util.Await
 import imcamilo.github.com.model.EQCL
 import imcamilo.github.com.service.EQCenterService
 import io.circe.generic.auto._
@@ -11,14 +11,13 @@ import io.finch.circe._
 import io.finch._
 import io.finch.syntax._
 
-import scala.collection.mutable.ListBuffer
-
 object Scismo extends App {
 
   lazy val eqService = EQCenterService
 
-  val dataEarth: Endpoint[Future[ListBuffer[String]]] = get("time") {
-    Ok(eqService.lastInformation())
+  val dataEarth: Endpoint[String] = get("time") {
+    eqService.lastInformation()
+    Ok("fist endpoint!!")
   }
 
   val service: Service[Request, Response] = JsonpFilter.andThen(dataEarth.toServiceAs[Application.Json])
