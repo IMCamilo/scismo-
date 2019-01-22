3ec1c16267eff18b955e4cb7d949cc8d510c6791
Camilo <camilo.jorquera.a@gmail.com>
2019-01-22 12:11:54 -0300

    hotfix(return future endpoint)

--- src/main/scala/imcamilo/github/com/Scismo.scala
+++ src/main/scala/imcamilo/github/com/Scismo.scala
@@ -3,7 +3,7 @@ package imcamilo.github.com
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.filter.JsonpFilter
-import com.twitter.util.Await
+import com.twitter.util.{Await, Future}
 import imcamilo.github.com.model.EQCL
 import imcamilo.github.com.service.EQCenterService
 import io.circe.generic.auto._
@@ -11,13 +11,14 @@ import io.finch.circe._
 import io.finch._
 import io.finch.syntax._
 
+import scala.collection.mutable.ListBuffer
+
 object Scismo extends App {
 
   lazy val eqService = EQCenterService
 
-  val dataEarth: Endpoint[String] = get("time") {
-    eqService.lastInformation()
-    Ok("wena!")
+  val dataEarth: Endpoint[Future[ListBuffer[String]]] = get("time") {
+    Ok(eqService.lastInformation())
   }
 
   val service: Service[Request, Response] = JsonpFilter.andThen(dataEarth.toServiceAs[Application.Json])
